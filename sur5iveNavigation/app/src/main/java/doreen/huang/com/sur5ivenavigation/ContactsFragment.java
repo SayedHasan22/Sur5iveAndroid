ce3d48b9241b2db84fe9318a5ce77139acf76374
JKawwa <jad.kawwa@gmail.com>
2018-03-26 19:23:40 -0400

    Added Stubs for other Fragments

---
+++ sur5iveNavigation/app/src/main/java/doreen/huang/com/sur5ivenavigation/ContactsFragment.java
@@ -0,0 +1,48 @@
+package doreen.huang.com.sur5ivenavigation;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import doreen.huang.com.sur5ivenavigation.services.SharedCacheService;
+
+public class ContactsFragment extends Fragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_message,
+                container, false);
+
+        TextView txtNumber = view.findViewById(R.id.saveButton);
+        txtNumber.setOnClickListener(this);
+        txtNumber.getText();
+
+        List<Object> contactList = new ArrayList<>();
+
+        // Add to cache
+        //SharedCacheService.getInstance().getCache().put("ContactList", contactList);
+
+        // Get from cache
+        //contactList = (List<Object>) SharedCacheService.getInstance().getCache().get("ContactList");
+
+        return view;
+    }
+
+    public void setText(String text) {
+        //TextView view = (TextView) getView().findViewById(R.id.detailsText);
+        //view.setText(text);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this.getActivity().getApplicationContext(), "Text Field Pressed", Toast.LENGTH_SHORT).show();
+    }
+}
